321a9cb542acbd2e1c5b1482f8f8065804463096
yangqiong <yangqionggo@icloud.com>
2018-07-22 07:50:59 +0800

    init

--- themes/hiero/source/js/main.js
+++ themes/hiero/source/js/main.js
@@ -69,3 +69,14 @@ $('#content').on('click', function(){
       $('#main-navigation').removeClass('main-navigation-open');
     }
   });
+
+var _mtac = {"senseQuery":1};
+(function() {
+	var mta = document.createElement("script");
+	mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+	mta.setAttribute("name", "MTAH5");
+	mta.setAttribute("sid", "500626582");
+	mta.setAttribute("cid", "500626583");
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(mta, s);
+})();
